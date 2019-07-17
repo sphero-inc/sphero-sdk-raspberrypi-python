121f6e7efb18c40f5dc166da1f9596034230fc4b
Angel Campos <angel.campos@sphero.com>
2019-07-17 16:22:25 -0600

    more package import fun

--- sphero_sdk/__init__.py
+++ sphero_sdk/__init__.py
@@ -3,14 +3,6 @@ from sphero_sdk.aio.client.dal.serial_async_dal import SerialAsyncDal
 from sphero_sdk.observer.client.toys.observer_sphero_rvr import ObserverSpheroRvr
 from sphero_sdk.observer.client.dal.serial_observer_dal import SerialObserverDal
 
-from sphero_sdk.helpers.drive_helper import DriveHelper
-from sphero_sdk.helpers.drive_helper_enum import RawMotorModes
-from sphero_sdk.helpers.ir_helper import IrHelper
-from sphero_sdk.helpers.ir_helper_enum import IrCodes
-from sphero_sdk.helpers.leds_helper import LedsHelper
-from sphero_sdk.helpers.leds_helper_enum import RvrLedGroups
-from sphero_sdk.helpers.rgb_colors_enum import RgbColors
-
 from sphero_sdk.common.commands.api_and_shell import *
 from sphero_sdk.common.commands.connection import *
 from sphero_sdk.common.commands.drive import *
@@ -18,3 +10,11 @@ from sphero_sdk.common.commands.io import *
 from sphero_sdk.common.commands.power import *
 from sphero_sdk.common.commands.sensor import *
 from sphero_sdk.common.commands.system_info import *
+
+from sphero_sdk.helpers.rgb_colors_enum import RgbColors
+from sphero_sdk.helpers.ir_helper_enum import IrCodes
+from sphero_sdk.helpers.leds_helper_enum import RvrLedGroups
+from sphero_sdk.helpers.drive_helper_enum import RawMotorModes
+from sphero_sdk.helpers.leds_helper import LedsHelper
+from sphero_sdk.helpers.drive_helper import DriveHelper
+from sphero_sdk.helpers.ir_helper import IrHelper
--- sphero_sdk/helpers/__init__.py
+++
@@ -1,6 +0,0 @@
-from sphero_sdk.helpers.drive_helper import DriveHelper
-from sphero_sdk.helpers.drive_helper_enum import RawMotorModes
-from sphero_sdk.helpers.ir_helper import IrHelper
-from sphero_sdk.helpers.leds_helper import LedsHelper
-from sphero_sdk.helpers.rgb_colors_enum import RgbColors
-from sphero_sdk.helpers.leds_helper_enum import RvrLedGroups
