2676dea236a176cc4ed8d91a9302907adc2e6f4c
Connor Ghazaleh <connor.ghazaleh@sphero.com>
2019-07-25 15:41:33 -0600

    fixing imports in observer_sphero_rvr

--- sphero_sdk/observer/client/toys/observer_sphero_rvr.py
+++ sphero_sdk/observer/client/toys/observer_sphero_rvr.py
@@ -7,7 +7,8 @@
 
 import logging.config
 from sphero_sdk.observer.config import logging_config
-from sphero_sdk.observer.config import LogLevel
+# from sphero_sdk.observer.config import LogLevel
+from sphero_sdk.common.log_level import LogLevel
 from sphero_sdk.observer.observer_base import Observer
 from sphero_sdk.observer.client.dal.serial_observer_dal import SerialObserverDal
 from sphero_sdk.observer.client.dal.observer_parser import ObserverParser
