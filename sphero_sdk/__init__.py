3aa077df2b49194c9fd3e054226ab508183106ac
Connor Ghazaleh <connor.ghazaleh@sphero.com>
2019-07-25 15:47:33 -0600

    copied helpers into sphero_sdk/ and updated imports in __init__.py file to reflect helpers in new location

--- sphero_sdk/__init__.py
+++ sphero_sdk/__init__.py
@@ -11,10 +11,10 @@ from sphero_sdk.common.commands.power import *
 from sphero_sdk.common.commands.sensor import *
 from sphero_sdk.common.commands.system_info import *
 
-from sphero_sdk.helpers.rgb_colors_enum import RgbColors
-from sphero_sdk.helpers.ir_helper_enum import IrCodes
-from sphero_sdk.helpers.leds_helper_enum import RvrLedGroups
-from sphero_sdk.helpers.drive_helper_enum import RawMotorModes
-from sphero_sdk.helpers.async_leds_helper import AsyncLedsHelper
-from sphero_sdk.helpers.async_drive_helper import AsyncDriveHelper
-from sphero_sdk.helpers.async_ir_helper import AsyncIrHelper
+from sphero_sdk.helpers.colors_enums import Colors
+from sphero_sdk.helpers.infrared_codes_enums import InfraredCodes
+from sphero_sdk.helpers.rvr_led_groups_enum import RvrLedGroups
+# from sphero_sdk.helpers.drive_helper_enum import RawMotorModes
+from sphero_sdk.helpers.led_control_async import LedControlAsync
+from sphero_sdk.helpers.drive_control_async import DriveControlAsync
+from sphero_sdk.helpers.infrared_control_async import InfraredControlAsync
