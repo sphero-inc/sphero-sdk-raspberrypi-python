417ecf679890f8aa8b4330755ad4cb2fb42accd0
Angel Campos <angel.campos@sphero.com>
2019-07-26 16:43:26 -0600

    Updated public methods with doc strings

--- sphero_sdk/observer/client/dal/serial_observer_dal.py
+++ sphero_sdk/observer/client/dal/serial_observer_dal.py
@@ -13,11 +13,24 @@ class SerialObserverDal:
         pass
 
     def send_command(self, did, cid, target, timeout=None, inputs=[], outputs=[]):
+        """Creates a Message object using the provided parameters and sends it to the serial port.
 
+        Args:
+            did (uint8): Device ID
+            cid (uint8): Command ID
+            target (uint8): 1 - Nordic; 2 - ST
+            timeout (uint8): Time in seconds to wait for a response, if one is requested. Otherwise, ignored.
+            inputs (List(Parameter)): Inputs for command that is being sent
+            outputs (List(Parameter)): Expected outputs for command that is being sent
+
+        Returns:
+
+        """
         message = Message()
         message.did = did
         message.cid = cid
         message.target = target
+        message.is_activity = True
 
         if len(outputs) > 0:
             message.requests_response = True
@@ -31,39 +44,12 @@ class SerialObserverDal:
         self._port.send(message)
 
     def close(self):
+        """Closes the serial port, and joins the read/write thread.
+
+        Returns:
+
+        """
         self._port.close()
 
-    def on_command(self, did, cid, target, handler,
-                   timeout=None, outputs=None):
-        '''Provide a Handler Function for Data Coming Asynchronously from the Bot
-        That handler will operate in a separate Thread, so it's best to put any data
-        needed in the main thread into a queue.Queue object
-
-        For Example::
-            from queue import Queue
-            q = Queue()
-            def handler(output0, output1, ... outputN):
-                #operates in different thread
-                q.put((output0, output1, ... outputN))
-
-            # back in main thread
-            (output0, output1, ... outputN) = q.get()
-
-        Should probably create a new thread for this bad larry::
-            thread = Thread(
-                target=self.on_command,
-                args=(did, cid, target, handler),
-                kwargs=(
-                    timeout=timeout,
-                    outputs=[
-                        Parameter(name='output0', data_type='output0DataType', index=0),
-                        Parameter(name='output1', data_type='output1DataType', index=1),
-                        ...
-                        Parameter(name='outputN', data_type='outputNDataType', index=N),
-                    ]
-                )
-            )
-
-            thread.start()
-        '''
+    def on_command(self, did, cid, target, handler, timeout=None, outputs=None):
         raise NotImplementedError
