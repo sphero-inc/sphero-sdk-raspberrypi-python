4800abac4d8fd9ec0566260e3b661d8dfd0a4513
clara richter <clara.richter@sphero.com>
2019-07-29 11:08:24 -0600

    samples for sleep monitoring, ping, battery monitoring

---
+++ sphero_sdk/getting_started/observer/power/set_battery_state_notifications.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('/home/pi/raspberry-pi-python')
+
+import time
+from sphero_sdk import ObserverSpheroRvr
+
+rvr = ObserverSpheroRvr()
+
+def on_battery_voltage_state_change(state):
+    print("Battery voltage state changed; new state: ", state)
+
+
+def main():
+    """ This program demonstrates how to enable battery state change notifications and how to set up
+    a handler for such notifications.
+
+    """
+    rvr.wake()
+
+    # Instruct RVR to report battery change events
+    rvr.enable_battery_voltage_state_change_notify(is_enabled=True)
+
+    # Register handler to be called when a battery state change occur
+    rvr.on_battery_voltage_state_change_notify(on_battery_voltage_state_change)
+
+    rvr.close()
+
+
+
+main()
