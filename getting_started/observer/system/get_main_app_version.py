3fcb2ea9d7bc1496175039ab4d2f3eb1b7e247d0
Angel Campos <angel.campos@sphero.com>
2019-08-02 16:15:45 -0600

    adding short pause before script terminates

--- getting_started/observer/system/get_main_app_version.py
+++ getting_started/observer/system/get_main_app_version.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 
+import time
 from sphero_sdk import ObserverSpheroRvr
 
 rvr = ObserverSpheroRvr()
@@ -15,6 +16,8 @@ def main():
     rvr.get_main_application_version(handler, target=1)
 
     rvr.get_main_application_version(handler, target=2)
+
+    time.sleep(2)
 
     rvr.close()
 
