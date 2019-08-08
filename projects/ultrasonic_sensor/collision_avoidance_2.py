d442d0d91384f67543e31b37ee335dfa76ba2f31
Angel Campos <angel.campos@sphero.com>
2019-08-08 15:29:17 -0600

    removed one level from relative path

--- projects/ultrasonic_sensor/collision_avoidance_2.py
+++ projects/ultrasonic_sensor/collision_avoidance_2.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 import RPi.GPIO as GPIO
 import asyncio
