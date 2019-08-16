c33a248396df06f8c00b120771a8c4f6bc68a75f
Angel Campos <angel.campos@sphero.com>
2019-08-16 14:29:37 -0600

    adding shell script to install dependencies on Raspbian

---
+++ pi_dependencies/pidependencies.sh
@@ -0,0 +1,12 @@
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1
+python -m venv env
+source env/bin/activate
+pip install aiohttp
+pip install requests
+pip install websocket-client
+pip install pytest-asyncio
+pip install pytest
+pip install twine
+pip install pyserial
+pip install pyserial-asyncio
+y
