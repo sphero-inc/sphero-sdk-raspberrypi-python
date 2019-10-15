45aae4583013a555137913976dfd912c44409230
Angel Campos <angel.campos@sphero.com>
2019-10-15 15:07:25 -0600

    WIP startup script refactor

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,3 +1,4 @@
+pip install --upgrade pip
 pip install pipenv
 curl https://pyenv.run | bash
 echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
@@ -5,11 +6,4 @@ echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
 pipenv install
-
-if [ $? -eq 0 ]
-then
-  echo "RVR dependencies succesfully installed."
-else
-  pyenv install 3.5.3
-  pipenv install
-fi
+echo $?
