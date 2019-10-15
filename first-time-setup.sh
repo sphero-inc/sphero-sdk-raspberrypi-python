5be37e634c33ac5d034a217aa9e3c9461eadfa63
Angel Campos <angel.campos@sphero.com>
2019-10-15 16:42:28 -0600

    WIP startup script refactor

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,9 +1,11 @@
-pip install --upgrade pip
-pip install pipenv
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+python -m pip install pipenv
+echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
 curl https://pyenv.run | bash
 echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
-pyenv install 3.7.3
+pyenv install 3.5.3
 pipenv install
