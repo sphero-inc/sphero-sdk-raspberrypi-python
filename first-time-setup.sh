50cb38da0425ed8a1dc6705dacc90cb8e10884ab
Angel Campos <angel.campos@sphero.com>
2019-10-15 12:03:41 -0600

    modifying setup script with a fail condition

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,8 +1,15 @@
+pip install pipenv
 curl https://pyenv.run | bash
-touch ~/.bashrc
 echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
-pyenv install 3.5.3
 pipenv install
+
+if [ $? -eq 0 ]
+then
+  echo "RVR dependencies succesfully installed."
+else
+  pyenv install 3.5.3
+  pipenv install
+fi
