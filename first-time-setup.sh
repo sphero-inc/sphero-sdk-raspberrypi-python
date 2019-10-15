f62f2150c5b85ce10fa1512ecd8682e4af1dafba
Angel Campos <angel.campos@sphero.com>
2019-10-15 16:05:55 -0600

    WIP startup script refactor

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -5,5 +5,5 @@ echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
+pyenv install 3.7.3
 pipenv install
-echo $?
