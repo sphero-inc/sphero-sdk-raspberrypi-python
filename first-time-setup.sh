ca377f1ff2d4e48d2f53f7cb7985e2d9a129d688
Angel Campos <angel.campos@sphero.com>
2019-10-16 17:13:38 -0600

    removing duplicate source command

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -2,7 +2,6 @@ sudo apt-get update -y
 sudo apt-get install -y libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
 python -m pip install pipenv
 echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
-wait $(source ~/.bashrc)
 curl https://pyenv.run | bash
 echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
