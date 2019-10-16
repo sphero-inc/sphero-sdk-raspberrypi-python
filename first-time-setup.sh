5cde0958cca77353526bbb7485ed927b86896f75
Angel Campos <angel.campos@sphero.com>
2019-10-16 13:54:39 -0600

    removing uncessary installs

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,4 +1,3 @@
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
 python -m pip install pipenv
 echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
