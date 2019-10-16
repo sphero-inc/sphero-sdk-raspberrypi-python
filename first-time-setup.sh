73be5432535787d6c420b46e4fe7db6664e068b5
Angel Campos <angel.campos@sphero.com>
2019-10-16 16:25:43 -0600

    Adding wait command to resume script after bash restart

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,12 +1,12 @@
 sudo apt-get update -y
 sudo apt-get install -y libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
+python -m pip install pipenv
 echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
-python -m pip install pipenv
+curl https://pyenv.run | bash
 echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-curl https://pyenv.run | bash
+wait $(exec bash)
 pyenv install 3.5.3
 pipenv install
