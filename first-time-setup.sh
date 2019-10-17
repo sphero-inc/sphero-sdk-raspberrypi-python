3306a00d12e4024d1fd5b274c2b1b6454a46db33
Angel Campos <angel.campos@sphero.com>
2019-10-17 20:45:35 +0100

    adding xterm install to solve install issues

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,15 +1,10 @@
 sudo apt-get update -y
-sudo apt-get install -y libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
+sudo apt-get install -y xterm libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
 python -m pip install pipenv
 echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
 curl https://pyenv.run | bash
 echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-echo ''
-echo 'SUCCESS! Looks like your virtual environment toosl are ready.'
-echo 'Press ENTER to continue...'
-read
-wait $(source ~/.bashrc)
-pyenv install 3.5.3
-pipenv install
+source ~./bashrc
+xterm -e "pyenv install 3.5.3; pipenv install"
