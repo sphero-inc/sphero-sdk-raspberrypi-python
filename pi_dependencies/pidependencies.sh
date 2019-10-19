9f8278e5545cdf4b559edbf6f17f34e12909fdaf
Kinvert <keith@kinvert.com>
2019-10-19 11:37:32 -0400

    Change from pip to pip3
    I was getting an error for No module named 'aiohttp' and I manually installed that with pip3. My error then changed to No module named 'serial_asyncio'. So I am going to try this with all installations through pip3.

--- pi_dependencies/pidependencies.sh
+++ pi_dependencies/pidependencies.sh
@@ -1,11 +1,11 @@
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 python -m venv env
 source env/bin/activate
-pip install aiohttp
-pip install requests
-pip install websocket-client
-pip install pytest-asyncio
-pip install pytest
-pip install twine
-pip install pyserial
-pip install pyserial-asyncio
+pip3 install aiohttp
+pip3 install requests
+pip3 install websocket-client
+pip3 install pytest-asyncio
+pip3 install pytest
+pip3 install twine
+pip3 install pyserial
+pip3 install pyserial-asyncio
