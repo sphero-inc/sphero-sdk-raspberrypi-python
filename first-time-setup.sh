e41080f65088fc1facd66be0009879579ecd49a1
Angel Campos <angel.campos@sphero.com>
2019-10-22 11:13:32 -0600

    Adding a link to further instructions

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,10 +1,11 @@
 sudo apt-get update -y
-sudo apt-get install -y xterm libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
+sudo apt-get install -y libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
 python -m pip install pipenv
-echo "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
+printf "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
 curl https://pyenv.run | bash
-echo 'export PATH="/home/pi/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-xterm -e "pyenv install 3.5.3; pipenv install"
+printf 'export PATH="/home/pi/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)' >> ~/.bashrc
+printf '\n^^^ We just added the lines above to your .bashrc file for your convenience :) ^^^'
+printf '\nPlease head over to https://sdk.sphero.com/docs/getting_started/raspberry_pi/python_setup for some final setup instructions.'
+printf '\nLove,\nSphero\n'
+exec $SHELL
+
