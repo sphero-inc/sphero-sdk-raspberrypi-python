73008efed3901a72037739c937129ca49b34bc3c
Angel Campos <angel.campos@sphero.com>
2019-10-22 13:39:39 -0600

    Adding more dependencies

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get update -y
-sudo apt-get install -y libssl-dev bzip2 libreadline6 libreadline6-dev sqlite3
+sudo apt-get install -y make llvm build-essential libssl-dev bzip2 zlib1g-dev libbz2-dev libreadline6 libreadline6-dev sqlite3 \
+libreadline-dev libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
 python -m pip install pipenv
 printf "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
 curl https://pyenv.run | bash
@@ -8,4 +9,3 @@ printf '\n^^^ We just added the lines above to your .bashrc file for your conven
 printf '\nPlease head over to https://sdk.sphero.com/docs/getting_started/raspberry_pi/python_setup for some final setup instructions.'
 printf '\nLove,\nSphero\n'
 exec $SHELL
-
