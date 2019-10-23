a5bc002084b4ecedfe3629ca05e5866842393266
Angel Campos <angel.campos@sphero.com>
2019-10-23 16:44:41 -0600

    adding newline at the end of bashrc insert

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -2,5 +2,5 @@ sudo apt-get update -y
 sudo apt-get install -y make build-essential llvm libssl-dev bzip2 zlib1g-dev libbz2-dev libreadline6 libreadline-dev \
 libsqlite3-dev libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 python -m pip install pipenv
-printf "PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
+printf "PATH=$HOME/.local/bin:$PATH\n" >> ~/.bashrc
 exec $SHELL
