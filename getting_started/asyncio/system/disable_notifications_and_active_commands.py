d3da4bf84526f96bf9dbafd9668b88259bce1102
Rabia Aslam <rabia@sphero.com>
2020-05-29 16:49:51 -0600

    Updated and example project

--- getting_started/asyncio/system/disable_notifications_and_active_commands.py
+++ getting_started/asyncio/system/disable_notifications_and_active_commands.py
@@ -20,6 +20,9 @@ rvr = SpheroRvrAsync(
 async def imu_handler(imu_data):
     print('IMU data response: ', imu_data)
 
+async def color_detected_handler(color_detected_data):
+    print('Color detection data response: ', color_detected_data)
+
 
 async def main():
     """ This program demonstrates how to disable all notifications and active commands
@@ -34,6 +37,14 @@ async def main():
     await rvr.sensor_control.add_sensor_data_handler(
         service=RvrStreamingServices.imu,
         handler=imu_handler
+    )
+    await rvr.sensor_control.start(interval=1000)
+
+    print('Initiate color detection...')
+    await rvr.enable_color_detection(is_enabled=True)
+    await rvr.sensor_control.add_sensor_data_handler(
+        service=RvrStreamingServices.color_detection,
+        handler=color_detected_handler
     )
     await rvr.sensor_control.start(interval=1000)
 
