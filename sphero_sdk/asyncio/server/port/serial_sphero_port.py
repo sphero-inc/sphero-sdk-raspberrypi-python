60be4034e73c217c96d83eae142b383ffc22620b
Angel Campos <angel.campos@sphero.com>
2020-07-09 23:20:21 -0600

    Reverting changes to serial port

--- sphero_sdk/asyncio/server/port/serial_sphero_port.py
+++ sphero_sdk/asyncio/server/port/serial_sphero_port.py
@@ -45,15 +45,7 @@ class SerialSpheroPort(SpheroPortBase, asyncio.Protocol):
             msg (Message): Instance of Message
 
         """
-        # data = msg.serialise()
-        # print(data)
-        # data = b'\x8d\x1A\x02\x18\x4a\x01\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\xE7\xd8'  # get_temperature
-        data = b'\x8d\x1A\x01\x10\x0A\x01\x01\xC8\xd8'  # error 1
-        # data = b'\x8d\x1A\x01\x10\x09\x01\x02\xC8\xd8'  # error 2
-        # data = b'\x8d\x1A\x01\x10\x09\x01\x03\xC7\xd8'  # error 3
-        # data = b'\x8d\x1A\x01\x10\x09\x01\x04\xC6\xd8'  # error 4
-        # data = b'\x8d\x1A\x01\x10\x09\x01\x05\xC5\xd8'  # error 5
-        # data = b'\x8d\x1A\x01\x10\x09\x01\x06\xC4\xd8'  # error 6
+        data = msg.serialise()
         logger.debug('Writing serial data: [{}]'.format(', '.join('0x{:02x}'.format(x) for x in data)))
         self.__transport.write(data)
 
