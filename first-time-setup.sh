1397e5871e810dfae41e040ceb091d3112e2a98d
Jim Konish <jim.konish@sphero.com>
2021-05-19 17:10:18 +0100

    Update first-time-setup.sh to be more automatic

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update -y
 
 sudo apt-get install -y make \
@@ -17,15 +19,42 @@ tk-dev \
 libffi-dev \
 liblzma-dev
 
-python -m pip install pipenv
+pip3 install pipenv
 
-printf "PATH=$HOME/.local/bin:$PATH\n" >> ~/.profile
+# Reload ~/.profile.  In recent Raspberry Pi OS releases, this automatically includes $HOME/.local/bin in the path once it exists.
+source ~/.profile
+
+# Test whether pipenv is in the system path now
+if ! which pipenv ; then
+    # It's not in the path, so we should add it on boot
+    printf "\n# Add $HOME/.local/bin to PATH for pipenv.\n# This was automatically added by Sphero RVR SDK first-time-setup.sh\n" >> ~/.profile
+    printf "PATH=\"\$HOME/.local/bin:\$PATH\"\n" >> ~/.profile
+    
+    # Apply the ~/.profile update to this session as well 
+    source ~/.profile
+fi
+
+pipenv --python /usr/bin/python3.7
+
+# The cryptography package is a dependency, but for some reason pipenv can't always install it correctly, so we'll use pip instead.  ¯\_(ツ)_/¯
+pipenv run pip install cryptography
+
+pipenv install
 
 while true; do
-    read -p "A restart is required for changes to take effect.  Restart now?" yn
+    read -ep $'By default, the RVR SDK uses the UART on /dev/ttys0 to communicate with RVR.\nDo you have Serial Port enabled and Serial Shell disabled in Raspberry Pi Configuration?\n(y/n):' yn
     case $yn in
-        [Yy]* ) sudo reboot; break;;
-        [Nn]* ) exit;;
+        [Yy]* ) exit;;
+        [Nn]* ) 
+		read -p $'Would you like to open the command line raspi-config tool now to change these settings?\nYou can do this at any time with \'sudo raspi-config\'\n(y/n):' yn2
+		case $yn2 in 
+        		[Yy]* ) sudo raspi-config; break;;
+		        [Nn]* ) exit;;
+        		* ) echo "Please answer yes or no.";;
+    		esac 
+		break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+
