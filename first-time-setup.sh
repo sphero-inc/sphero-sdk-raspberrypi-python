493f4cdc4dca784fde8429c290d3a70bee020d90
Jim Konish <jim.konish@sphero.com>
2021-06-01 21:50:14 +0100

    Adjustments to first-time-setup.sh

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -2,8 +2,11 @@
 
 # This script guides a user through setting up the Sphero RVR Python SDK.
 
-# Install the SDK dependencies
-pip3 install -r requirements.txt
+# Install the SDK dependencies (only for the current user)
+pip3 install --user -r requirements.txt
+
+# Reload ~/.profile.  In recent Raspberry Pi OS releases, this automatically includes $HOME/.local/bin in the path once it exists.
+source ~/.profile
 
 # Provide an opportunity to correct the UART settings if needed.
 ./tools/pi-uart-check.sh
